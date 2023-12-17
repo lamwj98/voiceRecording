670473a22d6810664f8ede1c284cc1659c808922
Andy Lam <lamwj98@gmail.com>
2023-12-17 19:57:54 +0800

    First commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,118 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const audioRecorderPlayer = new AudioRecorderPlayer();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleRequestPermissions = async () => {
+    try {
+      const micResult = await request(PERMISSIONS.IOS.MICROPHONE);
+      if (micResult === 'granted') {
+        console.log('Microphone permission granted');
+      } else {
+        console.log('Microphone permission denied');
+      }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+      const mediaLibraryResult = await request(PERMISSIONS.IOS.MEDIA_LIBRARY);
+      if (mediaLibraryResult === 'granted') {
+        console.log('Media library permission granted');
+      } else {
+        console.log('Media library permission denied');
+      }
+    } catch (error) {
+      console.error('Error requesting permission: ', error);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  useEffect(() => {
+    handleRequestPermissions();
+  }, []);
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  const [isRecording, setIsRecording] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [recordSecs, setRecordSecs] = useState();
+  const [recordTime, setRecordTime] = useState<string | undefined>();
+  
+  const startRecording = async () => {
+    try {
+      console.log("Recording is starting ...");
+      setIsRecording(true);
+      const result = await audioRecorderPlayer.startRecorder();
+      audioRecorderPlayer.addRecordBackListener((e: any) => { 
+        setRecordSecs(e.current_position);
+        setRecordTime(audioRecorderPlayer.mmssss(Math.floor(e.current_position)));
+      });
+      console.log(result);
+      // Logic for starting recording
+      console.log("Recording started ...");
+    } catch (error) {
+      console.error('Error starting recording: ', error);
+    }
+
+  };
+
+  const stopRecording = async () => {
+    try {
+      console.log("Recording is stopping ...");
+      setIsRecording(true);
+      if (isPaused) {
+        setIsPaused(false);
+      }
+      // Logic for stopping recording
+      const result = await audioRecorderPlayer.stopRecorder();
+      audioRecorderPlayer.removeRecordBackListener();
+      setRecordSecs(undefined);
+      console.log(result);
+      console.log("Recording stopped ...");
+      } catch (error) {
+        console.error('Error stopping recording: ', error);
+      }
+  };
+
+  const pauseRecording = async () => {
+    try {
+      console.log("Recording is pausing ...");
+      setIsPaused(true);
+      // Logic for pausing recording
+      const result = await audioRecorderPlayer.pauseRecorder();
+      console.log(result);
+      console.log("Recording paused ...");
+    } catch (error) { 
+      console.error('Error pausing recording: ', error);
+    }
+  };
+
+  const resumeRecording = async () => {
+    try {
+      console.log("Recording is resuming ...");
+      setIsPaused(false);
+      // Logic for resuming recording
+      console.log("Recording resumed ...");
+    } catch (error) {
+      console.error('Error resuming recording: ', error);
+    }
+  };
+
+  return <View style={styles.container}>
+    <Text>Voice Memo Application</Text>
+    {isRecording && !isPaused && <Text>Recording in progress</Text>}
+    {isRecording && isPaused && <Text>Recording paused</Text>}
+    <Button 
+      title={isRecording ? "Stop Recording" : "Start Recording"}
+      onPress={isRecording ? stopRecording : startRecording}/>
+  </View>;
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ READ.md
@@ -0,0 +1,31 @@
+# Voice Recording Application
+
+This is an application for voice recording created by Github:@lamwj98.
+
+## Environment
+
+- Node >= 18.2.0
+- React-native 0.73.0
+
+Do make sure you have an ios emmulator installed on your computer or you have an ios device for running the application.
+
+## Installation
+
+Download the repository to your local computer, change directory into the repository base folder and run the following command to install the dependencies.
+```
+npm install 
+```
+After installing the dependencies, make sure you change directory into the ios folder and retrieve the pods for the project. This can be done by running the following commands.
+```
+cd ios
+pod install
+```
+
+## Running the application
+
+After installing the application, you can run the program from the base directory with the following command.
+```
+npm start
+```
+Upon starting up, press 'i' to launch the ios emmulator.
+
